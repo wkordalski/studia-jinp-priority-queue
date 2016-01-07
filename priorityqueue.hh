048a4e5fd1cf3f6c5c86f035a3c4375f08484bc5
Wojciech Kordalski <wojtek.kordalski@gmail.com>
2016-01-08 00:33:29 +0100

    Some minor changes.

--- priorityqueue.hh
+++ priorityqueue.hh
@@ -2,7 +2,6 @@
 #define _JNP1_PRIORITYQUEUE_HH_
 
 #include <cassert>
-#include <iostream>
 #include <exception>
 #include <map>
 #include <memory>
@@ -43,8 +42,7 @@ class PriorityQueue {
     using size_type = typename element_set<>::size_type;
 
    protected:
-    // Sorter classes
-    // Konstruktory i operatory przypisania muszą być nothrow
+    // Komparatory
     class KeyComparer {
        public:
         bool operator()(const key_ptr& lhs, const key_ptr& rhs) {
@@ -77,16 +75,12 @@ class PriorityQueue {
     // sortowanie po wartości, a potem po kluczu
     elements sorted_by_value;
     // sortowanie po kluczu, a potem po wartości, a na koniec po adresach
-    // (domyślnie)
     key_map sorted_by_key;
-
+    // set z wartoścami trzymanymi w kolejce
     value_set all_values;
 
    protected:
     element find_element(const key_ptr& k, const value_ptr& v) {
-        using namespace std;
-
-        // jeśli rzucą wyjątki, to trudno...
         auto kit = sorted_by_key.find(k);
         auto vit = all_values.find(v);
 
@@ -96,8 +90,6 @@ class PriorityQueue {
         return make_pair(kk, vv);
     }
     element find_element(const K& key, const V& value) {
-        // w razie czego usuwanie polega na nic nie robieniu,
-        // bo nie posiadamy key i value na własność
         auto k = std::make_shared<K>(key);
         auto v = std::make_shared<V>(value);
 
@@ -105,7 +97,6 @@ class PriorityQueue {
     }
 
    public:
-    // TODO: czy konstruktory na prawdę potrzebują jakiegoś exception-safety?
 
     // Konstruktor bezparametrowy tworzący pustą kolejkę [O(1)]
     PriorityQueue() = default;
@@ -113,7 +104,6 @@ class PriorityQueue {
     // Konstruktor kopiujący [O(queue.size())]
     // konstruktory kontenerów zapewniają silną gwarancję
     // jak coś się rzuci, to i tak PriorityQueue się nie utworzy
-    // TODO: zweryfikować
     PriorityQueue(const PriorityQueue<K, V>& queue) = default;
 
     // Konstruktor przenoszący [O(1)]
@@ -124,40 +114,37 @@ class PriorityQueue {
 
     // Operator przypisania [O(queue.size()) dla użycia P = Q, a O(1) dla użycia
     // P = move(Q)]
-    // TODO: w treści była inna definicja operatora przypisania,
-    // ale chyba trzeba rozbić ją na dwa konstruktory
     PriorityQueue<K, V>& operator=(const PriorityQueue<K, V>& queue) {
         if (this == &queue) return *this;
-        PriorityQueue<K, V> tmp(queue);  // może rzucać tylko bad_alloc
-        this->swap(tmp);                 // powinno być noexcept
+        PriorityQueue<K, V> tmp(queue);
+        this->swap(tmp);
         return *this;
     }
 
     PriorityQueue<K, V>& operator=(PriorityQueue<K, V>&& queue) noexcept(true) {
-        this->sorted_by_value =
-            std::move(queue.sorted_by_value);  // powinno być noexcept (move)
-        this->sorted_by_key =
-            std::move(queue.sorted_by_key);  // powinno być noexcept (move)
+        this->sorted_by_value = std::move(queue.sorted_by_value);
+        this->sorted_by_key = std::move(queue.sorted_by_key);
         this->all_values = std::move(queue.all_values);
-        // jest noexcept bo kopiowanie shared_ptr jest noexcept
         return *this;
     }
 
     // Metoda zwracająca true wtedy i tylko wtedy, gdy kolejka jest pusta [O(1)]
-    bool empty() const noexcept(true) { return sorted_by_value.empty(); }
+    bool empty() const noexcept { return sorted_by_value.empty(); }
 
     // Metoda zwracająca liczbę par (klucz, wartość) przechowywanych w kolejce
     // [O(1)]
-    size_type size() const noexcept(true) { return sorted_by_value.size(); }
+    size_type size() const noexcept { return sorted_by_value.size(); }
 
     // Metoda wstawiająca do kolejki parę o kluczu key i wartości value
     // [O(log size())] (dopuszczamy możliwość występowania w kolejce wielu
     // par o tym samym kluczu)
     void insert(const K& key, const V& value) {
-        using namespace std;
+        using std::make_pair;
+        using std::tie;
+
         key_ptr k;
         value_ptr v;
-        std::tie(k, v) = find_element(key, value);
+        tie(k, v) = find_element(key, value);
 
         auto pair_by_value = make_pair(k, v);
 
@@ -175,11 +162,9 @@ class PriorityQueue {
             it1 = sorted_by_value.insert(pair_by_value);
             al1 = true;
 
-            std::tie(it2, al2) =
-                sorted_by_key.insert(std::make_pair(k, value_map()));
+            tie(it2, al2) = sorted_by_key.insert(make_pair(k, value_map()));
 
-            std::tie(it3, al3) =
-                it2->second.insert(std::make_pair(v, element_set<>()));
+            tie(it3, al3) =  it2->second.insert(make_pair(v, element_set<>()));
 
             it4 = it3->second.insert(pair_by_value);
             al4 = true;
@@ -192,7 +177,7 @@ class PriorityQueue {
             if (al3) it2->second.erase(it3);
             if (al2) sorted_by_key.erase(it2);
             if (al1) sorted_by_value.erase(it1);
-            throw;
+          throw;
         }
     }
 
@@ -202,12 +187,10 @@ class PriorityQueue {
     // strukturze powinien zostać zgłoszony wyjątek PriorityQueueEmptyException
     const V& minValue() const {
         if (empty()) throw PriorityQueueEmptyException();
-        // begin() i * - noexcept(true)
         return *(sorted_by_value.begin()->second);
     }
     const V& maxValue() const {
         if (empty()) throw PriorityQueueEmptyException();
-        // rbegin() i * - noexcept(true)
         return *(sorted_by_value.rbegin()->second);
     }
 
@@ -217,64 +200,56 @@ class PriorityQueue {
     // PriorityQueueEmptyException
     const K& minKey() const {
         if (empty()) throw PriorityQueueEmptyException();
-        // begin() i * - noexcept(true)
         return *(sorted_by_value.begin()->first);
     }
     const K& maxKey() const {
         if (empty()) throw PriorityQueueEmptyException();
-        // rbegin() i * - noexcept(true)
         return *(sorted_by_value.rbegin()->first);
     }
 
     // Metody usuwające z kolejki jedną parę o odpowiednio najmniejszej lub
     // największej wartości [O(log size())]
     void deleteMin() {
-        if (empty()) return;                            // noexcept
-        const element& e = *(sorted_by_value.begin());  // noexcept
+        if (empty()) return;
+        const element& e = *(sorted_by_value.begin());
         value_ptr v = e.second;
 
-        auto kit =
-            sorted_by_key.find(e.first);  // może rzucać operator porównania
+        auto kit = sorted_by_key.find(e.first);
         assert(kit != sorted_by_key.end());
-        auto vit =
-            kit->second.find(e.second);  // może rzucać operator porównania
+        auto vit = kit->second.find(e.second);
         assert(vit != kit->second.end());
-        auto ait =  // nie rzuca
-            vit->second.begin();
+        auto ait = vit->second.begin();
         assert(ait != vit->second.end());
         auto bit = all_values.find(e.second);
         assert(bit != all_values.end());
 
         // Modyfikacje
-        vit->second.erase(ait);                             // noexcept
-        if (vit->second.empty()) kit->second.erase(vit);    // noexcept
-        if (kit->second.empty()) sorted_by_key.erase(kit);  // noexcept
-        sorted_by_value.erase(sorted_by_value.begin());     // noexcept
+        vit->second.erase(ait);
+        if (vit->second.empty()) kit->second.erase(vit);
+        if (kit->second.empty()) sorted_by_key.erase(kit);
+        sorted_by_value.erase(sorted_by_value.begin());
         all_values.erase(bit);
     }
 
     void deleteMax() {
-        if (empty()) return;                              // noexcept
-        const element& e = *prev(sorted_by_value.end());  // noexcept
+        if (empty()) return;
+        const element& e = *prev(sorted_by_value.end());
         value_ptr v = e.second;
 
-        auto kit =
-            sorted_by_key.find(e.first);  // może rzucać operator porównania
+        auto kit = sorted_by_key.find(e.first);
         assert(kit != sorted_by_key.end());
-        auto vit =
-            kit->second.find(e.second);  // może rzucać operator porównania
+        auto vit = kit->second.find(e.second);
         assert(vit != kit->second.end());
-        auto ait =  // nie rzuca
-            vit->second.begin();
+        auto ait = vit->second.begin();
         assert(ait != vit->second.end());
         auto bit = all_values.find(e.second);
         assert(bit != all_values.end());
 
         // Modyfikacje
-        vit->second.erase(ait);                              // noexcept
-        if (vit->second.empty()) kit->second.erase(vit);     // noexcept
-        if (kit->second.empty()) sorted_by_key.erase(kit);   // noexcept
-        sorted_by_value.erase(prev(sorted_by_value.end()));  // noexcept
+        vit->second.erase(ait);
+        if (vit->second.empty()) kit->second.erase(vit);
+        if (kit->second.empty()) sorted_by_key.erase(kit);
+        sorted_by_value.erase(prev(sorted_by_value.end()));
         all_values.erase(bit);
     }
 
@@ -284,15 +259,13 @@ class PriorityQueue {
     // PriorityQueueNotFoundException(); w przypadku kiedy w kolejce jest kilka
     // par
     // o kluczu key, zmienia wartość w dowolnie wybranej parze o podanym kluczu
-
-    // sprawdza, czy do danej pary odwołuje się tylko jeden zestaw wskaźników
-    // inaczej musi zaalokować nową parę (by modyfikacja nie dosięgła innych
-    // par)
     void changeValue(const K& key, const V& value) {
-      using namespace std;
+      using std::make_pair;
+      using std::tie;
+
       key_ptr k;
       value_ptr v;
-      std::tie(k, v) = find_element(key, value);
+      tie(k, v) = find_element(key, value);
 
       auto kit = sorted_by_key.find(k);
       if(kit == sorted_by_key.end()) throw PriorityQueueNotFoundException();
@@ -321,11 +294,9 @@ class PriorityQueue {
           it1 = sorted_by_value.insert(pair_by_value);
           al1 = true;
 
-          std::tie(it2, al2) =
-              sorted_by_key.insert(std::make_pair(k, value_map()));
+          tie(it2, al2) = sorted_by_key.insert(make_pair(k, value_map()));
 
-          std::tie(it3, al3) =
-              it2->second.insert(std::make_pair(v, element_set<>()));
+          tie(it3, al3) = it2->second.insert(make_pair(v, element_set<>()));
 
           it4 = it3->second.insert(pair_by_value);
           al4 = true;
@@ -352,13 +323,16 @@ class PriorityQueue {
     // wszystkie elementy z kolejki queue i wstawia je do kolejki *this
     // [O(size() + queue.size() * log (queue.size() + size()))]
     void merge(PriorityQueue<K, V>& queue) {
+      using std::tie;
+
       if (this == &queue) return;
 
       PriorityQueue<K, V> merged_queue = *this;
+
       for (element e : queue.sorted_by_value) {
           key_ptr k;
           value_ptr v;
-          std::tie(k, v) = merged_queue.find_element(e.first, e.second);
+          tie(k, v) = merged_queue.find_element(e.first, e.second);
 
           merged_queue.sorted_by_value.insert(e);
           merged_queue.sorted_by_key[k][v].insert(e);
